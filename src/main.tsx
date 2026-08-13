d8c9ae9de4f469d2ad3c8882cb46a202662f2c78
homeleadconnectllc <285241474+homeleadconnectllc@users.noreply.github.com>
2026-08-13 05:47:32 -0400

    style: load HLC agent team presentation

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./styles/workspace-nav.css";
+import "./styles/agent-team.css";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext";
 
