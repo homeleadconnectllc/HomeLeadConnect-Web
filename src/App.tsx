60be3ac14bd35fa44b0c4a876df378cc6faf2a88
homeleadconnectllc <285241474+homeleadconnectllc@users.noreply.github.com>
2026-08-19 06:31:38 -0400

    feat: mount mobile side controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import AppRouter from "./routes/AppRouter";
 import GlobalPullToRefresh from "./components/GlobalPullToRefresh";
 import GlobalSmartCompose from "./components/GlobalSmartCompose";
+import MobileViewControls from "./components/MobileViewControls";
 
 function App() {
   return (
     <>
       <GlobalPullToRefresh />
       <GlobalSmartCompose />
+      <MobileViewControls />
       <AppRouter />
     </>
   );
