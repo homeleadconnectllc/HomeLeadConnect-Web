58fca31cee04e22fe8c7d8b0129521c4cb7f4c37
homeleadconnectllc <285241474+homeleadconnectllc@users.noreply.github.com>
2026-08-23 14:23:14 -0400

    Mount final global presentation authority last

--- src/styles/AuthenticatedStyles.tsx
+++ src/styles/AuthenticatedStyles.tsx
@@ -1,6 +1,7 @@
 import "./authenticated-entry";
 import "./launch-live-device-authority.css";
 import "./manual-communications-launch-authority.css";
+import "./launch-global-presentation-authority.css";
 
 export default function AuthenticatedStyles() {
   return null;
