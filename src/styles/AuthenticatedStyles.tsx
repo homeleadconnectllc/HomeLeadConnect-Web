4bdfc4988ae55ec9212c610b19ba6bc6f5621c3d
homeleadconnectllc <285241474+homeleadconnectllc@users.noreply.github.com>
2026-08-24 20:40:53 -0400

    Post-launch: load desktop visual system last

--- src/styles/AuthenticatedStyles.tsx
+++ src/styles/AuthenticatedStyles.tsx
@@ -4,6 +4,7 @@ import "./manual-communications-launch-authority.css";
 import "./launch-global-presentation-authority.css";
 import "./final-live-screenshot-hotfix.css";
 import "./final-visual-punch.css";
+import "./desktop-visual-system.css";
 
 export default function AuthenticatedStyles() {
   return null;
