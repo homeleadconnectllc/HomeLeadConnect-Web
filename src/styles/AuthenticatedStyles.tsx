8f172ebc1361ed5d923c73a79a0f1dcd708e91de
homeleadconnectllc <285241474+homeleadconnectllc@users.noreply.github.com>
2026-09-04 19:13:39 -0400

    Visual: preserve Messages Lane 2 as final compact authority

--- src/styles/AuthenticatedStyles.tsx
+++ src/styles/AuthenticatedStyles.tsx
@@ -83,8 +83,8 @@ import "./version-a-legacy-light-surface-closure-20260904.css";
 import "./version-a-authenticated-physical-closure-20260904.css";
 import "./version-a-global-contrast-closure-20260904.css";
 import "./version-a-global-family-authority-20260904.css";
-import "./messages-lane-2-mobile-authority.css";
 import "./version-a-image2-final-authority-20260904.css";
+import "./messages-lane-2-mobile-authority.css";
 
 export default function AuthenticatedStyles() {
   return <MobileNavigationDialogAccessibility />;
