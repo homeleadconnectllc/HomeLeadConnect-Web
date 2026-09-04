9732064cfc87da5b8f217fe53e4c6800d5237a99
homeleadconnectllc <285241474+homeleadconnectllc@users.noreply.github.com>
2026-09-04 19:09:26 -0400

    Visual: load Image 2 authority last

--- src/styles/AuthenticatedStyles.tsx
+++ src/styles/AuthenticatedStyles.tsx
@@ -84,6 +84,7 @@ import "./version-a-authenticated-physical-closure-20260904.css";
 import "./version-a-global-contrast-closure-20260904.css";
 import "./version-a-global-family-authority-20260904.css";
 import "./messages-lane-2-mobile-authority.css";
+import "./version-a-image2-final-authority-20260904.css";
 
 export default function AuthenticatedStyles() {
   return <MobileNavigationDialogAccessibility />;
