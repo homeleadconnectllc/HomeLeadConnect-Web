97debcdca9b876128ac27ac48ede365dd40e32eb
homeleadconnectllc <285241474+homeleadconnectllc@users.noreply.github.com>
2026-09-04 19:50:34 -0400

    Repair white-island cascade without displacing Messages authority

--- src/styles/AuthenticatedStyles.tsx
+++ src/styles/AuthenticatedStyles.tsx
@@ -84,8 +84,8 @@ import "./version-a-authenticated-physical-closure-20260904.css";
 import "./version-a-global-contrast-closure-20260904.css";
 import "./version-a-global-family-authority-20260904.css";
 import "./version-a-image2-final-authority-20260904.css";
-import "./messages-lane-2-mobile-authority.css";
 import "./live-white-island-eradication-20260905.css";
+import "./messages-lane-2-mobile-authority.css";
 
 export default function AuthenticatedStyles() {
   return <MobileNavigationDialogAccessibility />;
