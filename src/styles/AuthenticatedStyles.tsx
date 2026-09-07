190684bdc741fdb44494f3fdee9973378ca15651
homeleadconnectllc <285241474+homeleadconnectllc@users.noreply.github.com>
2026-09-07 01:55:04 -0400

    Load exact-head mobile lead geometry correction last

--- src/styles/AuthenticatedStyles.tsx
+++ src/styles/AuthenticatedStyles.tsx
@@ -99,6 +99,7 @@ import "./global-visual-cleanup-physical-20260905.css";
 import "./authenticated-shared-mobile-surface-authority-20260905.css";
 import "./academy-help-physical-closure-20260906.css";
 import "./master-authenticated-visual-system-20260907.css";
+import "./master-authenticated-visual-certification-fix-20260907.css";
 
 export default function AuthenticatedStyles() {
   return <MobileNavigationDialogAccessibility />;
