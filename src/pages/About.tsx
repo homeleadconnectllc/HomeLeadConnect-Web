3e0d9547bba53a874741045315b3fb25aedf8c56
homeleadconnectllc <285241474+homeleadconnectllc@users.noreply.github.com>
2026-09-19 03:47:30 -0400

    Use single public visual authority in About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,6 @@
 import { Link } from "react-router-dom";
 import PublicSiteNav from "../components/PublicSiteNav";
 import { pageImage } from "../config/publicPageImagery";
-import "../styles/public-premium.css";
-import "../styles/public-board-pages-20260912.css";
-import "../styles/public-owner-visual-authority-20260918.css";
 import "../styles/public-owner-visual-final-20260918.css";
 
 const cards = [
