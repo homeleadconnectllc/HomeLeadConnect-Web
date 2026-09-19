f3ed2b58d16965e4e19ee9cf3ab0313bb0e33ec7
homeleadconnectllc <285241474+homeleadconnectllc@users.noreply.github.com>
2026-09-19 03:51:53 -0400

    Join ProfessionalApplication.tsx to canonical public visual family

--- src/pages/ProfessionalApplication.tsx
+++ src/pages/ProfessionalApplication.tsx
@@ -4,7 +4,7 @@ import PublicSiteNav from "../components/PublicSiteNav";
 import { pageImage } from "../config/publicPageImagery";
 import { submitProfessionalApplication } from "../api/professionalApplications";
 import { errorMessage } from "../lib/errorMessage";
-import "../styles/public-owner-visual-final-20260918.css";
+import "../styles/public-visual-family-20260919.css";
 
 const commonTrades = ["Handyman / home repair","HVAC / heating / cooling","Plumbing","Electrical","Roofing","Painting","Drywall / finishing","Cleaning / housekeeping","Moving / hauling","Landscaping / lawn care","Flooring","Carpentry","Siding / gutters","General contractor","Subcontractor"];
 export default function ProfessionalApplication(){
