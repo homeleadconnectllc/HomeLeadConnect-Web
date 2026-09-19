f88ac53bd2672c9231a32f4533fcc84ac46cbdc4
homeleadconnectllc <285241474+homeleadconnectllc@users.noreply.github.com>
2026-09-19 03:47:47 -0400

    Use single public visual authority in PublicJourney.tsx

--- src/pages/PublicJourney.tsx
+++ src/pages/PublicJourney.tsx
@@ -2,9 +2,6 @@ import { Link } from "react-router-dom";
 import PublicSiteNav from "../components/PublicSiteNav";
 import { pageImage } from "../config/publicPageImagery";
 import PathwayPage from "./PathwayPage";
-import "../styles/public-premium.css";
-import "../styles/public-board-pages-20260912.css";
-import "../styles/public-owner-visual-authority-20260918.css";
 import "../styles/public-owner-visual-final-20260918.css";
 
 type PublicJourneyKey = "services" | "pricing" | "trust" | "professionals" | "demo";
